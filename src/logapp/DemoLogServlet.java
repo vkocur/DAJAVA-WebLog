0756712b0c62d30f78fa01f20210bd34e2e0abbe
vladimir.kocur <vladimir.kocur@ness.com>
2018-11-17 14:49:13 +0100

    Initial revision

---
+++ src/logapp/DemoLogServlet.java
@@ -0,0 +1,48 @@
+package logapp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Servlet implementation class DemoLogServlet
+ */
+@WebServlet("/DemoLogServlet")
+public class DemoLogServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private final static Logger log = LogManager.getLogger(DemoLogServlet.class);
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoLogServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		log.info("Test log message");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
